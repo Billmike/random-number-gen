03d6d8c59fa9761d1ab01b6c6520af9e8b50454b
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-01-29 13:43:13 +0100

    Add travis to repo

---
+++ src/components/Pagination/Pagination.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'
+import Paginate from './';
+
+test('<Pagination />', () => {
+  const component = renderer.create(<Paginate />);
+  let tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
